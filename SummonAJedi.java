4025963a10bba626c358af3f30350192af707843
Tobioguntase <tobioguntuase@gmail.com>
2025-07-17 17:30:06 -0500

    adding user input to SummonAJedi

--- SummonAJedi.java
+++ SummonAJedi.java
@@ -1,11 +1,34 @@
+import java.util.Scanner;
+
 public class SummonAJedi {
     public static void main(String[] args) {
-        Jedi jedi = JediSummoner.summonJedi("Rey"); 
+        Scanner s = new Scanner(System.in); 
+        String name; 
+        int action;
 
-        jedi.greeting();
-        jedi.protectSummoner();
+        System.out.println("Which Jedi would you like to summon?"); 
+        System.out.println("Choose from the followng: ");
+        System.out.println("Luke");
+        System.out.println("Rey");
+        System.out.println("Anikan");
+        name = s.nextLine(); 
+        Jedi jedi = JediSummoner.summonJedi(name); 
 
+        System.out.println("" + name + " has arrived!" );
+        System.out.println("press 1 to greet or 2 to ask for help: ");
+        action = s.nextInt(); 
 
+        switch (action) {
+            case 1:
+                jedi.greeting();
+            
+            case 2: 
+                jedi.protectSummoner(); 
+                
+            default:
+                break;
+        }
+        s.close();
     }
 }
 
